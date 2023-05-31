91c00fa60be0db58af51746d0ceef72973cb5feb
Spadinsky <lechi666.92@gmail>
2023-06-01 00:10:45 +0300

    1.02

--- my.js
+++ my.js
@@ -1,5 +1,17 @@
 const { gulpfile } = require("gulp-cli/lib/shared/cli-options")
 
+//Создание нового проекта с помощью Gitkraken
+//        git init //начало слежения за файлами   
+//        git add -A //дорбавление всех файлов в репозиторий
+//        git commit -a -m" " //создание контрольной точки
+//Create repositories with github(он должен быть обязательно публичным)
+//  git remote add origin https://github.com/Lechi666/work.git
+//  git branch -M main
+//  git push -u origin main
+//Чтобы удалить,нужно сразу файлы удалить,потом репозиторий,только тогда из Gitkraken исчезнет
+
+
+
 //Подключаем gulp
 В пустой папке npm unit.Там пишем название сборки,и enter до конца
 Появляется файл package.json
