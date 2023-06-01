cdd874aa5a9629f277063c07be572a7664b52384
Spadinsky <lechi666.92@gmail>
2023-06-01 19:37:49 +0300

    1.01

--- my.js
+++ my.js
@@ -1,14 +1,15 @@
 const { gulpfile } = require("gulp-cli/lib/shared/cli-options")
 
-//Создание нового проекта с помощью Gitkraken
-//        git init //начало слежения за файлами   
-//        git add -A //дорбавление всех файлов в репозиторий
-//        git commit -a -m" " //создание контрольной точки
-//Create repositories with github(он должен быть обязательно публичным)
+//*Создание нового проекта с помощью Gitkraken
+//        git init               //начало слежения за файлами   
+//        git add -A             //дорбавление всех файлов в репозиторий
+//        git commit -a -m" "    //создание контрольной точки
+// Create repositories with github(он должен быть обязательно публичным)
 //  git remote add origin https://github.com/Lechi666/work.git
 //  git branch -M main
 //  git push -u origin main
-//Чтобы удалить,нужно сразу файлы удалить,потом репозиторий,только тогда из Gitkraken исчезнет
+// и открываем через gitkraken репозиторий папку на компьютере
+// Чтобы удалить,нужно сразу файлы удалить,потом репозиторий,только тогда из Gitkraken исчезнет
 
 
 
